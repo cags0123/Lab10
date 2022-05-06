034d5d605b4342d0b1a1b0d1183598bdaf232387
cags0123 <cg0123@sunysullivan.edu>
2022-05-05 23:49:05 -0400

    LAB10

---
+++ apply/scripts/script.js
@@ -0,0 +1,7 @@
+function content() {
+ var text = document.getElementById("new");
+ text.textContent = "Hi. I am a new paragraph that was created with
+a JavaScript function.";
+ text.style.color = "#c0145f";
+ text.style.fontSize = "2em";
+ }
---
+++ extend/scripts/script.js
@@ -0,0 +1,11 @@
+/*
+    Student Name: Christian Sandoval
+    File Name: script.js
+    Date: May 4, 2022
+*/
+
+function remove {
+  $("article > p).hide();
+
+function display {
+  $("article > p).show();
